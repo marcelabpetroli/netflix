fa5fd0b8a4c107cc3f8f03bc7a6994940af75254
Monica Hidalgo Martin <monicahidalgomartin@gmail.com>
2022-12-20 11:18:34 +0100

    Start endpoint to listen to server

--- src/index.js
+++ src/index.js
@@ -8,6 +8,13 @@ server.use(express.json());
 
 // init express aplication
 const serverPort = 4000;
+
+server.get('/movies', (req, res) => {
+  console.log('XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX');
+  console.log(req);
+  res.json({ message: 'ha sido un exito' });
+});
+
 server.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
 });
