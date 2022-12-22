472494ba882b11eaa1eb4cdc32706f8ca34502cd
Monica Hidalgo Martin <monicahidalgomartin@gmail.com>
2022-12-22 11:27:43 +0100

    Add url detail of movie

--- src/index.js
+++ src/index.js
@@ -9,11 +9,17 @@ server.use(express.json());
 
 // init express aplication
 const serverPort = 4000;
+server.listen(serverPort, () => {
+  console.log(`Server listening at http://localhost:${serverPort}`);
+});
 
+//Instalación del motor de plantillas
+server.set('view engine', 'ejs');
+
+//End points
 server.get('/movies', (req, res) => {
   const genderFilterParam = req.query.gender;
   const sortFilterParam = req.query.sort;
-
   function compare(a, b) {
     if (a.title < b.title) {
       return -1;
@@ -23,32 +29,40 @@ server.get('/movies', (req, res) => {
     }
     return 0;
   }
-
-  // const sortedMoviesAsc = moviesData.sort(compare);
-
-  // const sortedMoviesDesc = moviesData.reverse();
-
-  const filteredMoviesByGender = moviesData.filter((movies) => movies.gender.toLowerCase() === genderFilterParam);
-
+  const filteredMoviesByGender = moviesData.filter(
+    (movie) => movie.gender.toLowerCase() === genderFilterParam
+  );
   if (genderFilterParam === '') {
     res.json({
       success: true,
-      movies: sortFilterParam === 'asc' ? moviesData.sort(compare) : moviesData.reverse(compare),
+      movies:
+        sortFilterParam === 'asc'
+          ? moviesData.sort(compare)
+          : moviesData.reverse(compare),
     });
   } else {
     res.json({
       success: true,
-      movies: sortFilterParam === 'asc' ? filteredMoviesByGender.sort(compare) : filteredMoviesByGender.reverse(compare),
+      movies:
+        sortFilterParam === 'asc'
+          ? filteredMoviesByGender.sort(compare)
+          : filteredMoviesByGender.reverse(compare),
     });
   }
 });
 
-server.listen(serverPort, () => {
-  console.log(`Server listening at http://localhost:${serverPort}`);
+server.get('/movie/:movieId', (req, res) => {
+  const urlMovieId = req.params.movieId;
+  const foundMovie = moviesData.find((movie) => movie.id === urlMovieId);
+  res.render('movie', foundMovie);
 });
 
+//Servidores estáticos
 const staticServer = './src/public-react';
 server.use(express.static(staticServer));
 
 const staticServerImages = './src/public-movies-images';
 server.use(express.static(staticServerImages));
+
+const staticServerStyles = './src/public-css';
+server.use(express.static(staticServerStyles));
